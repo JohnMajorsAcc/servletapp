9a8270d80b0dffef8e2760cce3099dcf1f288375
John Major <demj_s@mail.ru>
2020-07-24 01:46:01 +0700

    Initial commit

---
+++ src/main/java/javaleaning/MainServlet.java
@@ -0,0 +1,53 @@
+package javaleaning;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+@WebServlet(urlPatterns = {"/cool-servlet/*"})
+public class MainServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        log("Method init =)");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Method service enter\n");
+        super.service(req, resp);
+        resp.getWriter().write("Method service exit\n");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String requestURI = req.getRequestURI();
+        String params = formatParams(req);
+
+        resp.getWriter().write("Method doGet\nURI: " + requestURI + "\nParams: " + params + "\n") ;
+    }
+
+    private String formatParams(HttpServletRequest req) {
+        return req.getParameterMap()
+                .entrySet()
+                .stream()
+                .map(entry -> {
+                    String param = String.join(" and ", entry.getValue());
+                    return entry.getKey() + "=>" + param;
+                })
+                .collect(Collectors.joining("\n"));
+    }
+
+    @Override
+    public void destroy() {
+        log("Method destroy =)");
+    }
+}
+
