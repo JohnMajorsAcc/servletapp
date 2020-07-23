de7fb7e4a40b9ff98ec6d8046abfcdcedbc9b95b
John Major <demj_s@mail.ru>
2020-07-24 02:22:23 +0700

    add form for file sending and some changes

--- src/main/java/javaleaning/MainServlet.java
+++ src/main/java/javaleaning/MainServlet.java
@@ -7,8 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns = {"/cool-servlet/*"})
@@ -29,9 +27,18 @@ public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String requestURI = req.getRequestURI();
-        String params = formatParams(req);
 
-        resp.getWriter().write("Method doGet\nURI: " + requestURI + "\nParams: " + params + "\n") ;
+        String params = formatParams(req);
+        resp.getWriter().write("Method doGet\nURI: " + requestURI + "\nParams: " + params + "\n");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String requestURI = req.getRequestURI();
+        String params = formatParams(req);
+        resp.getWriter().write("Method doPost\nURI: " + requestURI + "\nParams: " + params + "\n");
+
     }
 
     private String formatParams(HttpServletRequest req) {
